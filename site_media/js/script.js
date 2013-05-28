05b8c42c00ab218813137efa75439d57752ff09a
Peng Zhong <peng@nylira.com>
2013-05-28 12:25:11 +0800

    cleanup html

--- site_media/js/script.js
+++ site_media/js/script.js
@@ -1,12 +1,1 @@
-$("ul#tour-slides").cycle({
-  fx: 'scrollHorz',
-  pager: '#tour-slides-pager',
-  prev: '#tour-slides-prev',
-  next: '#tour-slides-next',
-  speed: 500,
-  timeout: 10000,
-  pause: true,
-  pauseOnPagerHover: true
-});
-
-$("#section-tour-tabs").tabs();
+$(".section-tour-tabs").tabs();
